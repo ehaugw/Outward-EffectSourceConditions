386d5727b0a82931a8e17657dbafc013bde70a00
Eivind <ehaugw@gmail.com>
2022-11-01 15:29:41 +0100

    include sourceconditions for equipment and statuseffects

---
+++ SourceConditionalEffects/SourceConditionEquipment.cs
@@ -0,0 +1,40 @@
+﻿namespace EffectSourceConditions
+{
+    public class SourceConditionEquipment : SourceCondition
+    {
+        /// <summary>
+        /// The skill ID that a character must have learned for CharacterHasRequirement to return true
+        /// </summary>
+        public int RequiredItemID;
+        
+        /// <summary>
+        /// Set to true to require the character to not have the relevant skill
+        /// </summary>
+        public bool Inverted = false;
+
+        /// <summary>
+        /// Assigns the ID of the provided Skill to RequiredSkillID
+        /// </summary>
+        public Item RequiredSkill
+        {
+            get
+            {
+                return ResourcesPrefabManager.Instance.GetItemPrefab(RequiredItemID);
+            }
+            set
+            {
+                RequiredItemID = value.ItemID;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if RequiredSkillID <= 0 or if the character knows the Skill with ItemID = RequiredSkillID
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        public override bool CharacterHasRequirement(Character character)
+        {
+            return ((RequiredItemID == 0) || (((character?.Inventory?.Equipment?.ItemEquippedCount(RequiredItemID) ?? 0) > 0) ^ Inverted));
+        }
+    }
+}
---
+++ SourceConditionalEffects/SourceConditionStatusEffect.cs
@@ -0,0 +1,40 @@
+﻿namespace EffectSourceConditions
+{
+    public class SourceConditionStatusEffect : SourceCondition
+    {
+        /// <summary>
+        /// The skill ID that a character must have learned for CharacterHasRequirement to return true
+        /// </summary>
+        public string statusEffectName;
+        
+        /// <summary>
+        /// Set to true to require the character to not have the relevant skill
+        /// </summary>
+        public bool Inverted = false;
+
+        /// <summary>
+        /// Assigns the ID of the provided Skill to RequiredSkillID
+        /// </summary>
+        public StatusEffect RequiredStatusEffect
+        {
+            get
+            {
+                return ResourcesPrefabManager.Instance.GetStatusEffectPrefab(statusEffectName);
+            }
+            set
+            {
+                statusEffectName = value.IdentifierName;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if RequiredSkillID <= 0 or if the character knows the Skill with ItemID = RequiredSkillID
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        public override bool CharacterHasRequirement(Character character)
+        {
+            return (statusEffectName != null && (character?.StatusEffectMngr?.HasStatusEffect(statusEffectName) ?? false)) ^ Inverted;
+        }
+    }
+}

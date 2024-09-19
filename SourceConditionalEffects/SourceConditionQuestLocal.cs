9cfc25f61a4faa4af6e5cdbccaa261203b0234f0
Eivind <ehaugw@gmail.com>
2024-09-19 23:23:03 +0200

    implement SourceConditionQuestLocal

---
+++ SourceConditionalEffects/SourceConditionQuestLocal.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using TinyHelper;
+
+namespace EffectSourceConditions
+{
+    public class SourceConditionQuestLocal : SourceCondition
+    {
+        /// <summary>
+        /// The skill IDs that a character must have learned for CharacterHasRequirement to return true
+        /// </summary>
+        public int[] Quests;
+        
+        /// <summary>
+        /// Set to true to require the character to not have the relevant skill
+        /// </summary>
+        public bool Inverted = false;
+
+        public LogicType Logic = LogicType.Any;
+
+        /// <summary>
+        /// Returns true the character knows the Skill with ItemID = RequiredSkillID
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        public override bool CharacterHasRequirement(Character character)
+        {
+            return QuestRequirements.HasQuestKnowledgeLocal (character, Quests, Logic, Inverted);
+        }
+    }
+}

e316da058e1560152ba73fb7eded8a0144a066b0
Eivind <ehaugw@gmail.com>
2024-09-20 11:19:33 +0200

    make SourceConditionEquipment able to require enchanted equipment

--- SourceConditionalEffects/SourceConditionEquipment.cs
+++ SourceConditionalEffects/SourceConditionEquipment.cs
@@ -1,31 +1,23 @@
-﻿namespace EffectSourceConditions
+﻿using System.Linq;
+
+namespace EffectSourceConditions
 {
     public class SourceConditionEquipment : SourceCondition
     {
         /// <summary>
-        /// The skill ID that a character must have learned for CharacterHasRequirement to return true
+        /// The item ID that a character must have equipped for CharacterHasRequirement to return true
         /// </summary>
         public int RequiredItemID;
+
+        /// <summary>
+        /// The enchant ID that a character must have equipped for CharacterHasRequirement to return true
+        /// </summary>
+        public int RequiredEnchantID;
         
         /// <summary>
         /// Set to true to require the character to not have the relevant skill
         /// </summary>
         public bool Inverted = false;
-
-        /// <summary>
-        /// Assigns the ID of the provided Skill to RequiredSkillID
-        /// </summary>
-        public Item RequiredSkill
-        {
-            get
-            {
-                return ResourcesPrefabManager.Instance.GetItemPrefab(RequiredItemID);
-            }
-            set
-            {
-                RequiredItemID = value.ItemID;
-            }
-        }
 
         /// <summary>
         /// Returns true if RequiredSkillID <= 0 or if the character knows the Skill with ItemID = RequiredSkillID
@@ -34,7 +26,22 @@
         /// <returns></returns>
         public override bool CharacterHasRequirement(Character character)
         {
-            return ((RequiredItemID == 0) || (((character?.Inventory?.Equipment?.ItemEquippedCount(RequiredItemID) ?? 0) > 0) ^ Inverted));
+            if (character?.Inventory?.Equipment is CharacterEquipment characterEquipment)
+            {
+                foreach (var slot in characterEquipment.EquipmentSlots.Where(s => s != null && s.EquippedItem != null))
+                {
+                    var matchingItem    = RequiredItemID    == 0 || slot.EquippedItem.ItemID == RequiredItemID;
+                    var matchingEnchant = RequiredEnchantID == 0 || slot.EquippedItem.ActiveEnchantmentIDs.Contains(RequiredEnchantID);
+
+                    if (matchingItem && matchingEnchant)
+                    {
+                        return !Inverted;
+                    }
+                }
+            }
+
+            return Inverted;
+            //return ((RequiredItemID == 0) || (((character?.Inventory?.Equipment?.ItemEquippedCount(RequiredItemID) ?? 0) > 0) ^ Inverted));
         }
     }
 }

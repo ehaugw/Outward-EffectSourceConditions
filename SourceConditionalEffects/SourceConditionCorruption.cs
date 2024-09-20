e2e8610c5c9583c206a6ba2c1b9d46adf2f0a563
Eivind <ehaugw@gmail.com>
2024-09-20 20:16:35 +0200

    implement SourceConditionCorruption and SourceConditionItemInInventory

---
+++ SourceConditionalEffects/SourceConditionCorruption.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace EffectSourceConditions
+{
+    public class SourceConditionCorruption : SourceCondition
+    {
+        public float Corruption = 0;
+
+        /// <summary>
+        /// Set to true to require the character to not have the relevant skill
+        /// </summary>
+        public bool Inverted = false;
+
+        /// <summary>
+        /// Returns true if ManaCost <= 0 or if the character has equal to or more mana than required.
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        public override bool CharacterHasRequirement(Character character)
+        {
+            return ((character?.PlayerStats?.Corruption?? 0) >= Corruption) ^ Inverted;
+        }
+    }
+}
---
+++ SourceConditionalEffects/SourceConditionItemInInventory.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace EffectSourceConditions
+{
+    public class SourceConditionItemInInventory : SourceCondition
+    {
+        /// <summary>
+        /// The item ID that a character must have equipped for CharacterHasRequirement to return true
+        /// </summary>
+        public int RequiredItemID;
+
+        /// <summary>
+        /// The enchant ID that a character must have equipped for CharacterHasRequirement to return true
+        /// </summary>
+        public int Amount = 1;
+        
+        /// <summary>
+        /// Set to true to require the character to not have the relevant skill
+        /// </summary>
+        public bool Inverted = false;
+
+        /// <summary>
+        /// Returns true if RequiredSkillID <= 0 or if the character knows the Skill with ItemID = RequiredSkillID
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        public override bool CharacterHasRequirement(Character character)
+        {
+            return ((RequiredItemID == 0) || (((character?.Inventory?.ItemCount(RequiredItemID) ?? 0) > 0) ^ Inverted));
+        }
+    }
+}
